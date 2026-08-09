d4c32aabeac6004ffb46c9894993a4e37e59e571
luciferdennitsa62-boop <luciferdennitsa62@gmail.com>
2026-08-09 20:28:24 +0300

    Create HMCore.java

---
+++ src/main/java/hmcore/HMCore.java
@@ -0,0 +1,11 @@
+package hmcore;
+
+public class HMCore {
+
+    public static final String NAME = "HM Core Client";
+    public static final String VERSION = "1.0";
+
+    public static void init() {
+        System.out.println(NAME + " v" + VERSION + " loaded");
+    }
+}
