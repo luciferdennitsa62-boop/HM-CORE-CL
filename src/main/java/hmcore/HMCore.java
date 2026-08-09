be3edb2891cc629474e223e1a8b798570c310ef1
luciferdennitsa62-boop <luciferdennitsa62@gmail.com>
2026-08-09 20:38:27 +0300

    Update HMCore.java

--- src/main/java/hmcore/HMCore.java
+++ src/main/java/hmcore/HMCore.java
@@ -1,11 +1,26 @@
 package hmcore;
 
+import meteordevelopment.meteorclient.MeteorClient;
+
 public class HMCore {
 
     public static final String NAME = "HM Core Client";
     public static final String VERSION = "1.0";
 
+    private static boolean initialized = false;
+
     public static void init() {
-        System.out.println(NAME + " v" + VERSION + " loaded");
+        if (initialized) return;
+
+        initialized = true;
+
+        MeteorClient.LOG.info("================================");
+        MeteorClient.LOG.info("{} v{} loaded", NAME, VERSION);
+        MeteorClient.LOG.info("HM Core system initialized");
+        MeteorClient.LOG.info("================================");
+    }
+
+    public static boolean isInitialized() {
+        return initialized;
     }
 }
