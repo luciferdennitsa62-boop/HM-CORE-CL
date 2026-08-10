678f141d53921c8f05aa15f9e87b08a30daf3d88
luciferdennitsa62-boop <luciferdennitsa62@gmail.com>
2026-08-10 22:01:01 +0300

    Update HMManager.java

--- src/main/java/hmcore/core/HMManager.java
+++ src/main/java/hmcore/core/HMManager.java
@@ -3,7 +3,6 @@ package hmcore.core;
 import meteordevelopment.meteorclient.MeteorClient;
 
 public class HMManager {
-
     private static boolean initialized = false;
 
     public static void init() {
@@ -11,7 +10,10 @@ public class HMManager {
 
         initialized = true;
 
-        MeteorClient.LOG.info("[HM] Manager initialized");
+        MeteorClient.LOG.info("[HM] ===============================");
+        MeteorClient.LOG.info("[HM] HM CORE initializing...");
+        MeteorClient.LOG.info("[HM] Core manager started");
+        MeteorClient.LOG.info("[HM] ===============================");
     }
 
     public static boolean isInitialized() {
